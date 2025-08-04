73d72f91549556c2dd58344b32bfa1a70960eb6d
Marcinho <marcin1897@gmail.com>
2025-08-04 15:30:03 +0200

    Add logs for openai_api_key

--- src/main/java/com/example/languagelearning/LanguageLearningApplication.java
+++ src/main/java/com/example/languagelearning/LanguageLearningApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LanguageLearningApplication {
     public static void main(String[] args) {
+        System.out.println("OPENAI_API_KEY: " + System.getenv("OPENAI_API_KEY"));
         SpringApplication.run(LanguageLearningApplication.class, args);
     }
 } 
